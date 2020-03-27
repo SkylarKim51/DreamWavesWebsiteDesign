38ebee14c0f61158fe9ef1ecc180e6ec06dee2db
SkylarKim51 <32948931+SkylarKim51@users.noreply.github.com>
2020-03-27 13:38:52 -0700

    added date, first real use of java

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-'use strict'
+'use strict';
+console.log('Here\'s a hidden message');
+
+let today = new Date(); //declare new object
+let formatDate = today.toDateString(); //transform the date format, no time/timezone
+let selectElement = document.getElementById('date'); //retrieve element
+selectElement.innerHTML= formatDate; //insert string inside element
