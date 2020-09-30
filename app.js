38b17a4b81b70aa4f27b2d5e2347d6239438d16d
SkylarKim51 <32948931+SkylarKim51@users.noreply.github.com>
2020-09-29 19:00:09 -0700

    sticky navbar implemented

--- app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-console.log('Here\'s a hidden message');
-
-let today = new Date(); //declare new object
-let formatDate = today.toDateString(); //transform the date format, no time/timezone
-let selectElement = document.getElementById('date'); //retrieve element
-selectElement.innerHTML= formatDate; //insert string inside element
